43002b7f477ecf810e5b8f8b7c1c92fbe65fd7bd
wrpys <496732709@qq.com>
2017-08-06 22:28:06 +0800

    paths

--- src/main/webapp/src/js/app.js
+++ src/main/webapp/src/js/app.js
@@ -2,7 +2,10 @@
  * Created by wrp on 2017/8/6.
  */
 requirejs.config({
-    baseUrl: contextPath + '/src/js'
+    baseUrl: contextPath + '/src/js',
+    paths: {
+        'jquery': 'lib/jquery.js'
+    }
 });
 require(['helper'], function (helper) {
     var str = helper.trim("    amd        ");
--- src/main/webapp/src/js/helper.js
+++ src/main/webapp/src/js/helper.js
@@ -2,6 +2,7 @@
  * Created by wrp on 2017/8/6.
  */
 define('helper', ['jquery'], function ($) {
+    console.log($)
     return {
         trim: function (str) {
             return $.trim(str);
